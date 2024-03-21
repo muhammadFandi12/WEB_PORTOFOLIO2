f90fda0726082ee72c53ce73e6851495d3d7287a
muhammadFandi12 <fandijr56@gmail.com>
2024-03-21 23:22:35 +0800

    Portofolio2

---
+++ main.js
@@ -0,0 +1,8 @@
+document.addEventListener('DOMContentLoaded', function(){
+    var typed = new Typed('.header-text h1', {
+        strings: ["Web-Develoment", "Jasa-Joki","Trader"],
+        typeSpeed: 50,
+        backSpeed: 50,
+        loop: true
+    });
+});
